c49f6bfc8fcb5002fff176932704f07ca9e569b9
Miguel Olarte <mbsic91@gmail.com>
2025-09-15 17:45:26 -0500

    Feat: Implementation of ValidationExceptionHandler for the rest domain exceptions

--- domain/model/src/main/java/com/katusoft/model/vehicle/Vehicle.java
+++ domain/model/src/main/java/com/katusoft/model/vehicle/Vehicle.java
@@ -1,14 +1,4 @@
 package com.katusoft.model.vehicle;
-import lombok.Builder;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-//import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-//@NoArgsConstructor
-@AllArgsConstructor
-@Builder(toBuilder = true)
 public class Vehicle {
 }
--- infrastructure/entry-points/api-rest/src/main/java/com/katusoft/api/controller/ValidationExceptionHandler.java
+++ infrastructure/entry-points/api-rest/src/main/java/com/katusoft/api/controller/ValidationExceptionHandler.java
@@ -2,7 +2,11 @@ package com.katusoft.api.controller;
 
 
 import com.katusoft.api.dto.ErrorResponse;
+import com.katusoft.model.exception.InvalidEmailException;
+import com.katusoft.model.exception.InvalidPasswordException;
+import com.katusoft.model.exception.InvalidUsernameException;
 import com.katusoft.model.exception.UserAlreadyExistsException;
+import com.katusoft.model.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -43,7 +47,7 @@ public class ValidationExceptionHandler {
     return ResponseEntity.badRequest().body(response);
   }
 
-  // EXCEPCIONES DE DOMINIO - User Already Exists
+  // EXCEPCION DE DOMINIO - User Already Exists
   @ExceptionHandler(UserAlreadyExistsException.class)
   public ResponseEntity<ErrorResponse> handleUserAlreadyExists(UserAlreadyExistsException ex) {
 
@@ -56,4 +60,61 @@ public class ValidationExceptionHandler {
 
     return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
   }
+
+  //EXCEPCIÓN DE DOMINIO - Invalid Email Exception
+  @ExceptionHandler(InvalidEmailException.class)
+  public ResponseEntity<ErrorResponse> hanleInvalidEmail(InvalidEmailException ex) {
+
+    ErrorResponse repsonse = ErrorResponse.builder()
+        .timestamp(LocalDateTime.now())
+        .status(HttpStatus.BAD_REQUEST.value())
+        .error("Invalid Email")
+        .message(ex.getMessage())
+        .build();
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(repsonse);
+  }
+
+  //EXCEPCIÓN DE DOMINIO - Invalid Password Exception
+  @ExceptionHandler(InvalidPasswordException.class)
+  public ResponseEntity<ErrorResponse> handleInvalidPassword(InvalidPasswordException ex) {
+
+    ErrorResponse response = ErrorResponse.builder()
+        .timestamp(LocalDateTime.now())
+        .status(HttpStatus.BAD_REQUEST.value())
+        .error("Invalid Password")
+        .message(ex.getMessage())
+        .build();
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+  }
+
+  //EXCEPCIÓN DE DOMINIO - Invalid Username Exception
+  @ExceptionHandler(InvalidUsernameException.class)
+  public ResponseEntity<ErrorResponse> handleInvalidPassword(InvalidUsernameException ex) {
+
+    ErrorResponse response = ErrorResponse.builder()
+        .timestamp(LocalDateTime.now())
+        .status(HttpStatus.BAD_REQUEST.value())
+        .error("Invalid Username")
+        .message(ex.getMessage())
+        .build();
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+  }
+
+  //EXCEPCIÓN DE DOMINIO -  User Not Found Exception
+  @ExceptionHandler(UserNotFoundException.class)
+  public ResponseEntity<ErrorResponse> handleInvalidPassword(UserNotFoundException ex) {
+
+    ErrorResponse response = ErrorResponse.builder()
+        .timestamp(LocalDateTime.now())
+        .status(HttpStatus.BAD_REQUEST.value())
+        .error("Invalid Password")
+        .message(ex.getMessage())
+        .build();
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+  }
+
 }
