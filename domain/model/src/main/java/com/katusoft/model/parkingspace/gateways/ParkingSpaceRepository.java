3744932c54aedf9f184bbc474ef369126bde4ddf
Miguel Olarte <mbsic91@gmail.com>
2025-10-15 15:59:22 -0500

    Feat: ParkingSpace gateway has been added

--- domain/model/src/main/java/com/katusoft/model/parkingspace/gateways/ParkingSpaceRepository.java
+++ domain/model/src/main/java/com/katusoft/model/parkingspace/gateways/ParkingSpaceRepository.java
@@ -1,4 +1,21 @@
 package com.katusoft.model.parkingspace.gateways;
 
+import com.katusoft.model.fare.Type;
+import com.katusoft.model.parkingspace.ParkingSpace;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ParkingSpaceRepository {
+
+  ParkingSpace createParkingSpace(ParkingSpace parkingSpace);
+  Optional<ParkingSpace> findById(int id);
+  List<ParkingSpace> findAll();
+  Optional<ParkingSpace> findAvailableByType(Type type);
+  List<ParkingSpace> findAllAvailable();
+  void  deleteById(UUID id);
+  long countByType(Type type);
+  boolean existsById(UUID id);
+
 }
